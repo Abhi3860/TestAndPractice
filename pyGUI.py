0bbb774f3e90babcc9d290e616e9107aa0a422fa
Abhitosh Panigrahi <abhitosh386@gmail.com>
2026-01-25 14:33:53 +0530

    GUI

---
+++ pyGUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Test GUI")
+root.minsize(200,200)
+root.geometry('350x500')
+root.configure(background='#0096DC')
+root.mainloop()
